822930c0c6164071a3b60992ab9f5f46dde10704
Shreelatha <shreelathan@gmail.com>
2021-10-16 18:43:31 +0530

    and file completed

--- utils/all_utils.py
+++ utils/all_utils.py
@@ -1,3 +1,13 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import joblib # FOR SAVING MY MODEL AS A BINARY FILE
+from matplotlib.colors import ListedColormap
+import os
+
+plt.style.use("fivethirtyeight") # THIS IS STYLE OF GRAPHS
+
+
 def prepare_data(df):
   X = df.drop("y", axis=1)
 
