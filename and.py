0bfc49d6ec973d517df2ff8fdc2ae0cfc4989b2a
Shreelatha <shreelathan@gmail.com>
2021-10-16 20:16:30 +0530

    code organised

--- and.py
+++ and.py
@@ -1,28 +1,31 @@
 from utils.model import Perceptron
 from utils.all_utils import prepare_data, save_model, save_plot
 import pandas as pd
-import os
 
 
-AND = {
-    "x1": [0,0,1,1],
-    "x2": [0,1,0,1],
-    "y": [0,0,0,1],
-}
+def main(data, eta, epochs, filename, plotFilename):
+    df = pd.DataFrame(data)
 
-df = pd.DataFrame(AND)
+    print(df)
 
-print(df)
+    X,y = prepare_data(df)
 
-X,y = prepare_data(df)
+    model = Perceptron(eta=eta, epochs=epochs)
+    model.fit(X, y)
 
-ETA = 0.3 # 0 and 1
-EPOCHS = 10
+    _ = model.total_loss()
 
-model = Perceptron(eta=ETA, epochs=EPOCHS)
-model.fit(X, y)
+    save_model(model, filename=filename)
+    save_plot(df, plotFilename, model)
 
-_ = model.total_loss()
+if __name__ == "__main__":
+    AND = {
+        "x1": [0,0,1,1],
+        "x2": [0,1,0,1],
+        "y": [0,0,0,1],
+    }
 
-save_model(model, filename="and.model")
-save_plot(df, "and.png", model)
+    ETA = 0.3 # 0 and 1
+    EPOCHS = 10
+
+    main(data=AND, eta=ETA, epochs=EPOCHS, filename="and.model", plotFilename="and.png")
---
+++ or.py
@@ -0,0 +1,31 @@
+from utils.model import Perceptron
+from utils.all_utils import prepare_data, save_model, save_plot
+import pandas as pd
+
+
+def main(data, eta, epochs, filename, plotFilename):
+    df = pd.DataFrame(data)
+
+    print(df)
+
+    X,y = prepare_data(df)
+
+    model = Perceptron(eta=eta, epochs=epochs)
+    model.fit(X, y)
+
+    _ = model.total_loss()
+
+    save_model(model, filename=filename)
+    save_plot(df, plotFilename, model)
+
+if __name__ == "__main__":
+    AND = {
+        "x1": [0,0,1,1],
+        "x2": [0,1,0,1],
+        "y": [0,1,1,1],
+    }
+
+    ETA = 0.3 # 0 and 1
+    EPOCHS = 10
+
+    main(data=AND, eta=ETA, epochs=EPOCHS, filename="or.model", plotFilename="or.png")
